1162d933c6025e700a5be026634e0a7b17f7b186
ramosv <56368263+ramosv@users.noreply.github.com>
2022-11-01 19:10:38 -0600

    Quick Union: Java Implementation

---
+++ Part-1/Week-1/Union-Find/src/QuickUnion.java
@@ -0,0 +1,33 @@
+public class QuickUnion {
+    private int[] id;
+
+    //Set id of each object to itself (N array accesses)
+    public QuickUnion(int N) {
+        id = new int[N];
+        for (int i = 0; i < N; i++) id[i] = i;
+
+    }
+
+    // chase parent pointers until reach root (depth of i array accesses)
+    private int root(int i) {
+        while (i != id[i]) i = id[i];
+        return i;
+    }
+
+    //Check if p and q have same root (depth of p and q array accesses)
+    public boolean connected(int p, int q) {
+        return root(p) == root(q);
+    }
+
+    //Check root of p to point to root of q (depth of p and q array accesses)
+    public void union(int p, int q) {
+        int i = root(p);
+        int j = root(q);
+        id[i] = j;
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
