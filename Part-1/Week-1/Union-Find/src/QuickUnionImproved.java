40eaa42bfb93b4c83982f984bc31c7418015ac0c
ramosv <56368263+ramosv@users.noreply.github.com>
2022-11-02 17:52:26 -0600

    Quick Union: Weighted

--- Part-1/Week-1/Union-Find/src/QuickUnionImproved.java
+++ Part-1/Week-1/Union-Find/src/QuickUnionImproved.java
@@ -3,11 +3,16 @@ public class QuickUnionImproved {
     //Quick Union with Weights and path compression
 
     private int[] id;
+    private int[] sz;
 
     //Set id of each object to itself (N array accesses)
     public QuickUnionImproved(int N) {
         id = new int[N];
-        for (int i = 0; i < N; i++) id[i] = i;
+        sz = new int[N];
+        for (int i = 0; i < N; i++) {
+            id[i] = i;
+            sz[i] = 1;
+        }
 
     }
 
@@ -22,11 +27,21 @@ public class QuickUnionImproved {
         return root(p) == root(q);
     }
 
+
     //Check root of p to point to root of q (depth of p and q array accesses)
     public void union(int p, int q) {
         int i = root(p);
         int j = root(q);
-        id[i] = j;
+        if (i == j) return;
+        if (sz[i] < sz[j]) {
+
+            id[i] = j;
+            sz[j] += sz[i];
+        } else {
+
+            id[j] = i;
+            sz[i] += sz[j];
+        }
     }
 
     public static void main(String[] args) {
